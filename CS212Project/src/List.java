8ed043c3dbf28a7b95f7a99534c1bd8f244b3d81
8boda <8boda@8boda-pc>
2023-10-15 23:29:44 +0300

    this is the project

---
+++ CS212Project/src/List.java
@@ -0,0 +1,12 @@
+
+public interface List<T> {
+public boolean Last();  
+public boolean Full();  
+public boolean empty();  
+public void findFirst(); 
+public void findNext(); 
+public T retrive();
+public void ubdate(T Data);  
+public void insert(T Data);  
+public void remove();
+}
