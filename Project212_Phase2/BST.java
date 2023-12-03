3bd068da3d3ec1d3f5c8a354ec9523025da9ec5c
Abdulaziz-Altamimi <147829131+Abdulaziz-Altamimi@users.noreply.github.com>
2023-12-03 17:39:36 +0300

    Update BST.java

--- Project212_Phase2/BST.java
+++ Project212_Phase2/BST.java
@@ -93,7 +93,7 @@ public class BST<T> {
 
 	public void print_fname(String name) {
 		if(root==null) {
-			System.out.println("The BST is empty");
+			System.out.println("\nThe BST is empty\n");
 			return;
 		}
 		print_fname((BSTNode<Contact>)root,name); 
@@ -153,62 +153,57 @@ public class BST<T> {
 
 
 
-
 	public boolean removeKey(String k) {
 		// Search for k
-		String k1 = k;
-		BSTNode<T> p = root;
-		BSTNode<T> q = null; // Parent of p
-		while (p != null) {
-			if (k1.compareToIgnoreCase( p.key) <0) {
-				q =p;
-				p = p.left;
-			}
-			else if (k1.compareToIgnoreCase( p.key) > 0) {
-				q = p;
-				p = p.right;
-			}
-			else { 
-				// Found the key
-				// Check the three cases
-				if ((p.left != null) && (p.right != null)) { 
-					// Case 3: two children
-					// Search for the min in the right subtree
-					BSTNode<T> min = p.right;
-					q = p;
-					while (min.left != null) {
-						q = min;
-						min = min.left;
-					}
-					p.key = min.key;
-					p.data = min.data;
-					k1 = min.key;
-					p = min;
-					// Now fall back to either case 1 or 2
-				}
-			}
-			// The subtree rooted at p will change here
-			if (p.left != null) { // One child
-				p = p.left;
-			} else { // One or no children
-				p = p.right;
-			}
-			if (q == null) { // No parent for p, root must change
-				root = p;
-			} else {
-				if (k1.compareToIgnoreCase( q.key) <0) {
-					q.left = p;
-				} else {
-					q.right = p;
-				}
-			}
-			current = root;
-			return true;
-		}
-
-		return false; // Not found
+	      String k1 = k;
+	      BSTNode<T> p = root;
+	      BSTNode<T> q = null; // Parent of p
+	      while (p != null) {
+	         if (k1.compareToIgnoreCase( p.key) <0) {
+	            q =p;
+	            p = p.left;
+	         } else if (k1.compareToIgnoreCase( p.key) >0) {
+	            q = p;
+	            p = p.right;
+	         }
+	else { // Found the key
+	            // Check the three cases
+	            if ((p.left != null) && (p.right != null)) {
+			// Case 3: two children
+	               // Search for the min in the right subtree
+	               BSTNode<T> min = p.right;
+	               q = p;
+	               while (min.left != null) {
+	                  q = min;
+	                  min = min.left;
+	               }
+	               p.key = min.key;
+	               p.data = min.data;
+	               k1 = min.key;
+	               p = min;
+	               // Now fall back to either case 1 or 2
+	            }
+	//The subtree rooted at p will change here
+	            if (p.left != null) { // One child
+	               p = p.left;
+	            } else { // One or no children
+	               p = p.right;
+	            }
+	            if (q == null) { // No parent for p, root must change
+	               root = p;
+	            } else {
+	               if (k1.compareToIgnoreCase( q.key) <0) {
+	                  q.left = p;
+	               } else {
+	                  q.right = p;
+	               }
+	            }
+	            current = root;
+	            return true;
+	         } 
+	      }
+	      return false; // Not found
 	}
-
 	
 
 
